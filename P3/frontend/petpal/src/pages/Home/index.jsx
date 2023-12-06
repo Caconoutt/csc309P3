5224057ef2275cbe908e8712d28ae6a539ff95c3
alyssali <alyssamengyuan.li@mail.utoronto.ca>
2023-12-05 23:29:11 -0500

    c

--- P3/frontend/petpal/src/pages/Home/index.jsx
+++ P3/frontend/petpal/src/pages/Home/index.jsx
@@ -1,8 +1,33 @@
+import { Button } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import { useEffect } from 'react'
 
 const Home = () => {
-    return <>
-    <h1>HOME</h1>
-    </>;
+  useEffect(() => {
+    fetch(`/account/token/`, {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      mode: 'cors',
+      body: JSON.stringify({ username: 'seeker_1', password: 'alyssa347lovetea' }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.access, '=====')
+        window.localStorage.setItem('token', JSON.stringify(data.access))
+        console.log(localStorage.getItem('token'))
+      })
+  })
+
+  return (
+    <>
+      <h1>HOME</h1>
+      <Link to='/PetCreate'>pet create</Link>
+      <Link to='/PetDetail'>pet create</Link>
+    </>
+  )
 }
 
-export default Home;
+export default Home
