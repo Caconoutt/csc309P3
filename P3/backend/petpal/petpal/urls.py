1e6c059cbaddecaa4b7f6ca459bd7cabafd970ac
Kevin Wu <kevintqj@gmail.com>
2023-12-08 03:49:13 -0500

    edit url for image

--- P3/backend/petpal/petpal/urls.py
+++ P3/backend/petpal/petpal/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
+from .settings import MEDIA_ROOT
 
 urlpatterns = [
     #path('api/', include('api.urls')),
@@ -23,4 +25,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('pet/', include('pet.urls')),
     path('comment/', include('comment.urls')),
+    re_path(r'media/(?P<path>.*)$',serve,{'document_root':MEDIA_ROOT}),
 ]
