3f18b2f5d773e4adf45b3fbebf239377bfbd2a9f
xuexiaoying <camilla.xue@mail.utoronto.ca>
2023-12-07 21:50:55 -0500

    fix blogList

--- P3/frontend/petpal/src/pages/BlogList/index.jsx
+++ P3/frontend/petpal/src/pages/BlogList/index.jsx
@@ -4,20 +4,20 @@ import { useUserData } from '../../contexts/AuthContext';
 import './style.css';
 
 const BlogList = () => {
-    const [blogs, setBlogs] = useState([]);
+    const [blogs, setBlogs] = useState();
     const { token, user_id } = useUserData(); 
     const navigate = useNavigate();
     const { shelterID } = useParams();
     const [userType, setUserType] = useState('');
-    console.log(token)
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const userTypeRes = await fetch(`http://127.0.0.1:8000/account/usertype/`, {
+                const userTypeRes = await fetch(`http://localhost:8000/account/usertype/`, {
                     method: 'GET',
                     headers: {
-                    'Authorization': `Bearer ${token}`,
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${token}`,
                     },
                 });
                 if (!userTypeRes.ok) throw new Error('Error fetching user type');
@@ -27,14 +27,14 @@ const BlogList = () => {
             
                 let blogUrl = '';
                 if (userTypeJson.user_type === 'shelter') {
-                    blogUrl = `http://localhost:8000/shelter/blog/list/`;
+                    blogUrl = `http://localhost:8000/account/shelter/blog/list/`;
                 } else if (userTypeJson.user_type === 'seeker') {
                     blogUrl = `http://localhost:8000/shelter/${shelterID}/blog/list/`;
                 }
-
                 const response = await fetch(blogUrl, {
                     method: 'GET',
                     headers: {
+                        'Content-Type': 'application/json',
                         'Authorization': `Bearer ${token}`,
                     },
                 });
@@ -45,7 +45,7 @@ const BlogList = () => {
 
                 const blogData = await response.json();
                 setBlogs(blogData);
-                console.log(blogs)
+                
             } catch (error) {
                 console.error('Error fetching blogs:', error);
             }
@@ -58,20 +58,21 @@ const BlogList = () => {
 
     return (
         <div className="blog-list">
-            {blogs.map(blog => (
+             {blogs && 
+            blogs.map(blog => (
                 <div key={blog.id} className="blog-card">
                     <h3>{blog.title}</h3>
-                    <p>Author: {blog.owner.name}</p>
+                    {/* <p>Author: {blog.owner.username}</p> */}
                     <p>Created at: {new Date(blog.created_at).toLocaleString()}</p>
 
                     <div className="buttons">
                         <button className="details-button" onClick={() => navigate(`/ViewBlog/${blog.id}`)}>Details</button>
-                        {userType === 'shelter' && user_id === blog.owner.id && (
+                       {userType === 'shelter' && user_id === blog.owner.id && (
                             <button className="edit-button" onClick={() => navigate(`/EditBlog/${blog.id}`)}>Edit</button>
                         )}
                     </div>
                 </div>
-            ))}
+            ))} 
         </div>
     );
 }
