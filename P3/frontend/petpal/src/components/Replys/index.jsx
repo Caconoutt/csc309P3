4ed5934efecdb09dc252143ce48e279b8af40fdf
Christina0313 <christinalan20021023@gmail.com>
2023-12-09 04:55:58 -0500

    updated review list page

--- P3/frontend/petpal/src/components/Replys/index.jsx
+++ P3/frontend/petpal/src/components/Replys/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Review from '../Review';
+import Reply from '../Reply';
 
 const Replys = ({data}) =>{
 
@@ -10,7 +11,7 @@ const Replys = ({data}) =>{
               console.log(item.id);
               return (
                 <React.Fragment key={item.id}>
-                  <Review detail={item.detail} review_id={item.id} />
+                  <Reply detail={item.detail} review_id={item.id} />
                   {console.log(item.detail)}
                 </React.Fragment>
               );
